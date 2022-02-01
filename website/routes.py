338b633e27178032fbe6892b56dcec7903901a9d
Naveen Ailawadi <naveen.ailawadi91@gmail.com>
2022-02-01 08:35:48 -0500

    started connecting wallet

--- website/routes.py
+++ website/routes.py
@@ -1,13 +1,56 @@
 from website import app
-from flask import render_template
+from flask import render_template, request, Flask, g, send_from_directory, abort, jsonify
+from flask_jwt_extended import jwt_required, create_access_token, get_jwt_identity, set_access_cookies
+from eth_account.messages import defunct_hash_message
+from web3.auto import w3
+import time
 
 
 @app.route('/home')
 @app.route('/')
 def home():
+    # find a way to only show connect wallet if they are connected
     return render_template('home.html')
 
 
 @app.route('/connect-wallet')
 def connect_wallet():
     return render_template('connect-wallet.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+
+    print("[+] creating session")
+
+    print("info: " + (str(request.json)))
+
+    public_address = request.json[0]
+    signature = request.json[1]
+
+    test_string = "Avvenire"
+
+    rightnow = int(time.time())
+    sortanow = rightnow - rightnow % 600
+
+    original_message = 'Signing in to {} at {}'.format(test_string, sortanow)
+    message_hash = defunct_hash_message(text=original_message)
+    signer = w3.eth.account.recoverHash(message_hash, signature=signature)
+
+    if signer == public_address:
+        print("[+] this is fine " + str(signer))
+        # account.nonce = account.generate_nonce()
+        # db.session.commit()
+    else:
+        abort(401, 'could not authenticate signature')
+
+    print("[+] OMG looks good")
+
+    access_token = create_access_token(identity=public_address)
+
+    resp = jsonify({'login': True})
+    set_access_cookies(resp, access_token)
+    return resp, 200
+
+
+# use @jwt-required for mint function
---
+++ website/static/js/connect-wallet/metamask.js
@@ -0,0 +1,93 @@
+let provider;
+let accounts;
+
+let accountAddress = "";
+let signer;
+
+function login()
+{
+
+  console.log('oh hey there');
+
+ // signer.signMessage("hello");
+
+rightnow = (Date.now()/1000).toFixed(0)
+sortanow = rightnow-(rightnow%600)
+
+signer.signMessage("Signing in to Avvenire at "+sortanow, accountAddress, "")
+            .then((signature) => {               handleAuth(accountAddress, signature)
+            });
+}
+
+function handleAuth(accountAddress, signature)
+{
+  console.log(accountAddress);
+  console.log(signature);
+
+  fetch('login', {
+    method: 'post',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify([accountAddress,signature])
+  }).then((response) => {
+    return response.json();
+
+    // redirect to home
+  })
+  .then((data) => {
+    console.log(data);
+  });
+
+}
+
+function connect() {
+    console.log('connecting');
+    ethereum.enable().then(function () {
+
+        provider = new ethers.providers.Web3Provider(web3.currentProvider);
+
+
+        provider.getNetwork().then(function (result) {
+            if (result['chainId'] != 1) {
+                alert('Switch to Mainnet!');
+
+            } else { // okay, confirmed we're on mainnet
+
+                provider.listAccounts().then(function (result) {
+                    accountAddress = result[0]; // figure out the user's Eth address
+
+                    provider.getBalance(String(result[0])).then(function (balance) {
+                        var myBalance = (balance / ethers.constants.WeiPerEther).toFixed(4);
+                    });
+
+                    // get a signer object so we can do things that need signing
+                    signer = provider.getSigner();
+
+                    // log the user in
+                    login();
+
+                    // redirect to home
+                    // location.href = '/'
+                })
+            }
+        })
+    })
+
+}
+
+// connect by default
+connect();
+
+/*
+web3.eth.getAccounts()
+        .then((response) => {
+            const publicAddressResponse = response[0];
+
+            if (!(typeof publicAddressResponse === "undefined")) {
+                setPublicAddress(publicAddressResponse);
+                getNonce(publicAddressResponse);
+            }
+        })
+        .catch((e) => {
+            console.error(e);
+        });
+*/
