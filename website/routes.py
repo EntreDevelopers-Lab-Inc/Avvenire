5e1903a2505b3ad2c0d06b86e1dff5f028a858bf
Naveen Ailawadi <naveen.ailawadi91@gmail.com>
2022-02-01 07:33:46 -0500

    added wallet connect

--- website/routes.py
+++ website/routes.py
@@ -5,4 +5,9 @@ from flask import render_template
 @app.route('/home')
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
+
+
+@app.route('/connect-wallet')
+def connect_wallet():
+    return render_template('connect-wallet.html')
