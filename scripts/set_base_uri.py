96c8785ea9c893469a7b42fd2773be10f37b26a4
Daniel Yamagata <danielyamagata@Daniels-MacBook-Pro-7.local>
2022-02-09 01:18:57 -0500

    Wrote even more scripts...

--- scripts/set_base_uri.py
+++ scripts/set_base_uri.py
@@ -6,3 +6,7 @@ def set_base_uri(baseURI):
     avvenire_contract = AvvenireTest[-1]
     account = get_account()
     avvenire_contract.setBaseURI(baseURI, {"from": account})
+
+
+def main():
+    set_base_uri("https://ipfs.io/ipfs/QmUizisYNzj824jNxuiPTQ1ykBSEjmkp42wMZ7DVFvfZiK/")
