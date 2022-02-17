f0a49a1859e2dc0711378d12d6b6a94dd434c8af
Naveen Ailawadi <naveen.ailawadi91@gmail.com>
2022-02-17 09:10:54 -0500

    probably going to switch to ethereum instead of ethers

--- website/static/js/constants.js
+++ website/static/js/constants.js
@@ -4,3 +4,9 @@ const CHAIN_ID_INT = 4;
 
 // chain string: https://web3js.readthedocs.io/en/v1.2.11/web3-eth-contract.html
 const CHAIN_STRING = 'rinkeby';
+
+// infura test network
+const PROVIDER_ADDRESS = 'https://rinkeby.infura.io/v3/be3d0189bd6a4a66864b79793c50a1fd';
+
+// confirm blocks amount
+const CONFIRMED_BLOCKS = 1;
--- website/static/js/mint/mint.js
+++ website/static/js/mint/mint.js
@@ -1,1 +1,33 @@
-// get everything (including the contract from contract.js)
+// function to show an nft from ipfs
+function revealNFT(uri) {
+    // get the image address on the screen
+    var imageTag = $('#nft-img');
+
+    imageTag.setAttribute('src', uri);
+}
+
+// function to get an NFT's URI
+function getURI(tokenId) {
+
+}
+
+// function to mint nft
+function mintNFT() {
+    var contract = createContract();
+
+    // call the contract from the user's current address (this is just test code)
+    contract.createCollectible(123, 'https://gateway.pinata.cloud/ipfs/QmSsYRx3LpDAb1GZQm7zZ1AuHZjfbPkD6J7s9r41xu1mf8').then(function (transactionResponse) {
+        alert('created NFT transaction');
+
+        // wait for the event to respond
+        transactionResponse.wait(CONFIRMED_BLOCKS).then(function (transactionReceipt) {
+            alert('NFT creation confirmed!');
+            // once we have the receipt, we can show the image
+
+        });
+
+    });
+}
+
+
+// listen for event: https://docs.ethers.io/v5/api/contract/contract/#Contract--events
