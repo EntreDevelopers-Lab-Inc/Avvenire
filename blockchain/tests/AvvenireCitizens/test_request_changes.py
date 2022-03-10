c67eba496fe50a0e748fae90d0caf02925927e78
Daniel Yamagata <danielyamagata@Daniels-MacBook-Pro-7.local>
2022-03-10 00:55:06 -0500

    Fixed errors; added AvvenireCitizenMarket to the contract
    Tests should have been written to only called requestChange in AvvenireCitizen from the AvvenireCitizenMarket contract

--- blockchain/tests/AvvenireCitizens/test_request_changes.py
+++ blockchain/tests/AvvenireCitizens/test_request_changes.py
@@ -1,7 +1,7 @@
 import pytest
 import brownie
 
-from brownie import AvvenireTest, AvvenireCitizens, accounts
+from brownie import AvvenireTest, AvvenireCitizens, AvvenireCitizenMarket, accounts
 from web3 import Web3
 from scripts.helpful_scripts import get_account
 
@@ -16,31 +16,25 @@ def auction_set(fn_isolation):
 
 # try to change a token before it is mutable
 def test_token_change_before_mutable():
-    avvenire_contract = AvvenireTest[-1]
-    avvenire_citizens_contract = AvvenireCitizens[-1]
-    admin_account = get_account()
+    avvenire_auction_contract = AvvenireTest[-1]
+    avvenire_market_contract = AvvenireCitizenMarket[-1]
 
     # mint some nfts to account 2
     account = accounts[2]
 
     # make sure that the gas is less than 5% of the auction
     cost = Web3.toWei(1, "ether")
-    avvenire_contract.auctionMint(1, {"from": account, "value": cost})
-    drop_interval(1)
-
-    # end the auction
-    end_auction()
-    drop_interval(1)
+    avvenire_auction_contract.auctionMint(1, {"from": account, "value": cost})
 
     # request a change (will later be from an accessory contract) --> should fail, as not mutable
     with brownie.reverts():
-        assert avvenire_citizens_contract.requestChange(0, {"from": admin_account})
+        assert avvenire_market_contract.requestChange(0, {"from": account})
 
 
 # test the change of a non-existed token
 def test_non_existent_token_change():
     avvenire_contract = AvvenireTest[-1]
-    avvenire_citizens_contract = AvvenireCitizens[-1]
+    avvenire_market_contract = AvvenireCitizenMarket[-1]
 
     # mint some nfts to account 2
     admin_account = get_account()
@@ -51,14 +45,12 @@ def test_non_existent_token_change():
     avvenire_contract.auctionMint(1, {"from": account, "value": cost})
 
     # end the auction
-    end_auction()
+    end_auction_and_enable_changes()
     drop_interval(1)
-
-    avvenire_citizens_contract.setMutablityMode(True, {"from": admin_account})
 
     # request a change (will later be from an accessory contract) --> should fail, as not no token exists
     with brownie.reverts():
-        assert avvenire_citizens_contract.requestChange(1, {"from": admin_account})
+        assert avvenire_market_contract.requestChange(1, {"from": admin_account})
 
 
 # REQUEST CHANGE WILL ALWAYS THROW AN ERROR IF THE ACCOUNT IS NOT AN ALLOWED ACCOUNT
@@ -68,15 +60,13 @@ def test_non_existent_token_change():
 def test_request_by_nonowner():
     avvenire_contract = AvvenireTest[-1]
     avvenire_citizens_contract = AvvenireCitizens[-1]
+    avvenire_market_contract = AvvenireCitizenMarket[-1]
     admin_account = get_account()
 
     cost = Web3.toWei(1, "ether")
     avvenire_contract.auctionMint(1, {"from": accounts[1], "value": cost})
     avvenire_contract.auctionMint(1, {"from": accounts[2], "value": cost})
-    end_auction()
-
-    # Set mutability mode to true
-    avvenire_citizens_contract.setMutablityMode(True, {"from": admin_account})
+    end_auction_and_enable_changes()
 
     # Confirm that the owner of NFT #0 is Account[1]
     assert avvenire_citizens_contract.ownerOf(0) == accounts[1]
@@ -84,28 +74,26 @@ def test_request_by_nonowner():
 
     with brownie.reverts():
         # Try to change 0 from accounts[2] when the owner is accounts[1]
-        assert avvenire_citizens_contract.requestChange(0, {"from": accounts[2]})
+        assert avvenire_market_contract.requestChange(0, {"from": accounts[2]})
 
 
 def test_request_after_existing_request():
     avvenire_contract = AvvenireTest[-1]
     avvenire_citizens_contract = AvvenireCitizens[-1]
+    avvenire_market_contract = AvvenireCitizenMarket[-1]
     admin_account = get_account()
 
     cost = Web3.toWei(1, "ether")
     avvenire_contract.auctionMint(1, {"from": accounts[1], "value": cost})
-    end_auction()
-
-    # Set mutability mode to true
-    avvenire_citizens_contract.setMutablityMode(True, {"from": admin_account})
+    end_auction_and_enable_changes()
 
     # Confirm that the owner of NFT #0 is Account[1]
     assert avvenire_citizens_contract.ownerOf(0) == accounts[1]
-    avvenire_citizens_contract.requestChange(0, {"from": admin_account})
+    avvenire_market_contract.requestChange(0, {"from": accounts[1]})
 
     # Should throw error... Existing change request outstanding
     with brownie.reverts():
-        assert avvenire_citizens_contract.requestChange(0, {"from": admin_account})
+        assert avvenire_market_contract.requestChange(0, {"from": accounts[1]})
 
 
 # test the rest from an accessory contract
