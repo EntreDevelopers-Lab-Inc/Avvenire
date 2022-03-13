b9760f1e90b80fff0d6e393795fc249cb8f4681f
Naveen Ailawadi <naveen.ailawadi91@gmail.com>
2022-03-13 10:16:27 -0400

    reset art

--- blockchain/constants.py
+++ blockchain/constants.py
@@ -1,5 +1,5 @@
 BASE_URI = (
-    "https://gateway.pinata.cloud/ipfs/QmeVX67MmZpqqVhwKQzAYBdSk7TaXeGdkGrUifzi6H17Ej/"
+    "https://gateway.pinata.cloud/ipfs/QmRcNTkZopCYTW6N7AJM4aWTCtisUdTAbCQRwsxWfD4jSL/"
 )
 LOAD_URI = (
     "https://gateway.pinata.cloud/ipfs/Qme4pwMxwMJobSuTCqvczJCgmuM54EHBVtrEVqKtsjYWos"
