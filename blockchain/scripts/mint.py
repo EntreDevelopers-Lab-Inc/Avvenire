18d9596544d5e4b9a35775853354f45e800bd38d
Daniel Yamagata <danielyamagata@Daniels-MacBook-Pro-7.local>
2022-03-13 18:58:48 -0400

    cleaned up mint.py

--- blockchain/scripts/mint.py
+++ blockchain/scripts/mint.py
@@ -8,13 +8,7 @@ from scripts.auction import end_auction_and_enable_changes
 # mint some citizens to an account and end it
 def mint_citizens_and_end(amount, account):
     avvenire_auction_contract = AvvenireTest[-1]
-    cost = Web3.toWei(
-        amount, "ether")
-    avvenire_auction_contract.auctionMint(
-        amount, {"from": account, "value": cost})
-    drop_interval(1)
-
+    cost = Web3.toWei(amount, "ether")
+    avvenire_auction_contract.auctionMint(amount, {"from": account, "value": cost})
     # end the auction
-    current_auction_price = avvenire_auction_contract.getAuctionPrice()
     end_auction_and_enable_changes()
-    drop_interval(1)
