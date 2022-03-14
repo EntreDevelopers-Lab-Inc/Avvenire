acbc565f1bd3547253d2a3d6bcd9c395e7fbabec
Naveen Ailawadi <naveen.ailawadi91@gmail.com>
2022-03-14 10:46:30 -0400

    finished creating metadata for trait manager

--- blockchain/tools/TraitManager.py
+++ blockchain/tools/TraitManager.py
@@ -23,9 +23,9 @@ DEFAULT_FILE = f"DEFAULT.{EXTENSION}"
 
 # make a class that can create citizens by comparing ipfs to on-chain data
 class CitizenCreator:
-    def __init__(self, ipfs_traits, chain_data, sex):
+    def __init__(self, ipfs_data, chain_data, sex):
         # save each group of traits
-        self.ipfs_traits = ipfs_traits
+        self.ipfs_data = ipfs_data
 
         # save the overall on-chain data
         self.chain_data = chain_data
@@ -35,6 +35,9 @@ class CitizenCreator:
 
         # store the sex, so you can use the right file
         self.sex = sex
+
+        # will need the metadata for later
+        self.metadata = self.get_metadata()
 
     # create a function that converts traits to files
     # only going to get a list of data off the chain --> need to convert it somehow
@@ -69,20 +72,20 @@ class CitizenCreator:
                 return None
 
             return data
-        # if there is not a uri, need to check if it exists
+        # if there is not a uri, it can't exist (only traits with no change requested can be merged)
         elif trait[3]:
             # if it exists, just return a dictionary of unknown, as it will not be used
-            return {'file': 'unknown', 'attributes': {'trait_type': 'unknown', 'value': 'unknown'}}
+            return {'name': 'unknown', 'file': 'unknown', 'attributes': {'trait_type': 'unknown', 'value': 'unknown'}}
         # if no uri, and it doesn't exist, set it to the default
         else:
             # get what type of trait it is
             trait_type = TRAIT_ORDER[trait[5] - 1]
 
-            # continue here
+            # imply the file default information based on the trait type and sex
+            return {'name': 'Default', 'file': f"{trait_type}/{DEFAULT_FILE}", 'attributes': {'trait_type': trait_type, 'value': 'Default'}}
 
-    # create a property that gives you the mergable files
-    @property
-    def mergable_files(self):
+    # create a way to get the metadata
+    def get_metadata(self):
         # make sure that we have chain traits
         if not self.chain_traits:
             return None
@@ -90,38 +93,90 @@ class CitizenCreator:
         # create a file list
         files = []
 
-        # iterate over all of the files in both trait lists
-        for i in range(len(self.ipfs_traits)):
-            # get the ipfs trait
-            ipfs_trait = self.ipfs_traits[i]['file']
+        # create an attribute list (start with the sex data)
+        attributes = [{'trait_type': 'Sex', 'value': self.sex}]
 
-            # get the chain trait
-            chain_trait = self.chain_data[i]
+        # iterate over all of the files in both trait lists
+        for i in range(len(TRAIT_ORDER)):
+            # get the ipfs file
+            ipfs_file = self.ipfs_data['trait_files'][i]['file']
+            ipfs_attribute = self.ipfs_data['attributes'][i]
+
+            # get the chain file
+            chain_file = self.chain_traits[i]['file']
+            chain_attribute = self.chain_traits['name']
+
+            # figure out if the new trait exists
+            new_trait_exists = self.chain_data[i][3]
 
             # if the ipfs trait and the chain trait are the same, just use that file
-            if ipfs_trait == chain_trait:
-                files.append(ipfs_trait)
-            # if the ipfs file is a default and the new one is not, a trait has been added to the character
-            elif (ipfs_trait == DEFAULT_FILE) and (chain_trait != DEFAULT_FILE):
-                files.append(chain_trait)
-            # if the ipfs file is NOT a default, and the new one is, the trait has been removed, add the chain trait
-            elif (ipfs_trait != DEFAULT_FILE) and (chain_trait == DEFAULT_FILE):
-                # what about the case where the chain trait is changed for the first time?
-                # need to figure out if the chain trait exists, in which case, it should not be changed
-                if self.chain_data[i].exists:
-                    # the trait has been changed, go with the on-chain version
-                    files.append(ipfs_trait)
-                else:
-                    # the trait has been changed to the default, so go with the on-chain version
-                    files.append(chain_trait)
+            if ipfs_file == chain_file:
+                # set the file and attribute
+                new_file = ipfs_file
+                new_attribute = ipfs_attribute
+            # if the ipfs file is a default and the new one exists, use the chain trait
+            elif (ipfs_file == DEFAULT_FILE) and (new_trait_exists):
+                # set the file and attribute
+                new_file = chain_file
+                new_attribute = chain_attribute
+            # if the ipfs file is NOT a default, and the new one does not exist, the trait has been removed, add the chain trait
+            elif (ipfs_file != DEFAULT_FILE) and (not new_trait_exists):
+                # set the new trait to the default, which is the the current chain trait
+                # set the file and attribute
+                new_file = chain_file
+                new_attribute = chain_attribute
             # if the ipfs trait is not the same as the chain trait, the traits have been swapped
             else:
-                files.append(chain_trait)
+                # set the file and attribute
+                new_file = chain_file
+                new_attribute = chain_attribute
+
+            # add the new file to files
+            files.append({'trait_type': TRAIT_ORDER[i], 'file': new_file})
+
+            # add the new attribute to the attributes
+            attributes.append(new_attribute)
+
+            # make a metadata dict for the new citizen
+            metadata_dict = {
+                'name': self.ipfs_data['name'],
+                'image': '',  # this will be set AFTER the image is uploaded
+                'attributes': attributes,
+                'trait_files': files
+            }
+
+        return metadata_dict
+
+    # function to upload to ipfs (both citizen and metadata --> return metadata uri)
+    def upload_to_ipfs(self):
+        # get the files
+
+        # iterate over the files
+            # make the full path of the file, as this will indicate where to find the file for pasting
+
+            # paste each file onto the original piece of art
+
+        # create a hash using bytecode and upload it to the local pinata api: https://medium.com/python-pandemonium/getting-started-with-python-and-ipfs-94d14fdffd10
+
+        # upload the image hash to pinata: https://docs.pinata.cloud/api-pinning/pin-by-hash
+
+        # now that you have the pinata link, finish the metadata
+
+        # upload the metadata to local ipfs
+
+        # upload the file hash to pinata
+
+        # return the pinata metadata --> will only need to set the image uri of the character's data after this
+
+        pass
+
+    # get only the files for composition
+    @property
+    def composition_files(self):
+        # iterate over all the files and add them to a list
+        files = [file['file'] for file in self.metadata['trait_files']]
 
         return files
-
-    # create a property that gets the actual trait values (using the config and its mapping between trait groups)
-    # this would be faster as a database (for later)
 
 
 # make a market broker that interacts with the avvenire citizens market
@@ -157,7 +212,7 @@ class CitizenMarketBroker:
         ipfs_data = self.get_ipfs_data(citizen)
 
         # if there are not ipfs traits, say that we failed to update the citizen
-        if not ipfs_traits:
+        if not ipfs_data:
             print(
                 f"Failed to update citizen due to lack of ipfs data: {self.citizen_id}")
             return
@@ -166,7 +221,7 @@ class CitizenMarketBroker:
         sex = ipfs_data['attributes'][0]['value']
 
         # create a citizen creator with the two bunches of traits
-        citizen_creator = CitizenCreator(ipfs_data, citizen.traits, sex)
+        citizen_creator = CitizenCreator(ipfs_data, citizen[4], sex)
 
         # create the NEW citizen using generative art (using citizen creator)
 
