8db83103887cfe6d3d79cf98687dfe60d6e61ddf
Naveen Ailawadi <naveen.ailawadi91@gmail.com>
2022-03-24 17:58:50 -0400

    finished mint page

--- website/static/js/mint/mint.js
+++ website/static/js/mint/mint.js
@@ -1,5 +1,89 @@
 // need to verify with metamask: https://docs.metamask.io/guide/registering-function-names.html
 
+// get some constants
+var PRICE;  // figure out how to access structs
+
+
+// function to get the mint price
+// mint order: 1000 DA (3) --> WL (2) --> public (2000)
+// this should also set the min/max/value of the input button
+async function getMintPrice()
+{
+    // contract data
+    var config;
+    var price;
+    var whitelisted;
+
+    // input box data
+    var max;
+    var value;
+
+    // get the whitelist information
+    await CONTRACT.allowlist(ethereum.selectedAddress).then(function (resp) {
+        // set whether this address is whitelisted
+        whitelisted = ethers.utils.formatUnits(resp) != 0;
+    });
+
+    // get the sale information
+    await CONTRACT.saleConfig().then(
+        function (resp) {
+            config = resp;
+        });
+
+    // check if the auction is on with saleConfig.auctionSaleStartTime <= block.time_stamp
+    if (parseFloat(ethers.utils.formatEther(config[0])) > 0)  // this will be set to 0 when the auction is over
+    {
+        CONTRACT.AUCTION_START_PRICE().then(
+            function (resp) {
+                price = resp;
+            });
+
+        // set the max and value
+        max = 3;
+        value = max;
+    }
+    // check if the user is whitelisted and the price is greater than 0
+    else if ((parseFloat(ethers.utils.formatEther(config[2])) > 0) && (whitelisted))
+    {
+        // if the user is whitelisted saleConfig.mintlistPrice
+        price = config[2];
+
+        max = 2;
+        value = max;
+    }
+    // else, use the public sale price by calling saleConfig.publicPrice
+    else
+    {
+        price = config[3];
+
+        max = 2000;
+        value = 5;
+    }
+
+    // convert the price to eth from gwei
+    price = parseFloat(ethers.utils.formatEther(price));
+
+    // change the max and value
+    $('#amount').attr('max', max);
+    $('#amount').val(value);
+
+    // set the global variable
+    PRICE = price;
+}
+
+
+// function to set the total price
+async function setTotalCost()
+{
+    // get the amount
+    var amount = parseInt($('#amount').val());
+
+    // set the initial total cost
+    $('#total-cost').text(PRICE * amount);
+}
+
+
+
 // function to show an nft from ipfs
 function revealNFT(uri) {
     // get the image address on the screen
@@ -14,13 +98,16 @@ function getURI(tokenId) {
 }
 
 // function to mint nft
-async function mintNFTs(amount, gasLimit=GAS_LIMIT) {
+async function mintNFTs(gasLimit=GAS_LIMIT) {
+    // get the amount from the input box
+    var amount = $('#amount').val();
+
     // get the gas price
     var feeData = await CONTRACT_PROVIDER.getFeeData();  // may need to update to ethers 5.4 for this --> will see
     var maxFeePerGas = parseInt(ethers.utils.formatUnits(feeData.maxFeePerGas, "gwei"));
 
     // get the total cost = price * amount purchased + allowable gas cost in ETH
-    var totalCost = (PRICE * amount) + (maxFeePerGas * gasLimit / GWEI_PER_ETH);
+    var totalCost = (PRICE * amount)/* + (maxFeePerGas * gasLimit / GWEI_PER_ETH)*/;
 
     // make the total cost a string for the parse ether utilty
     totalCost = totalCost.toString();
@@ -29,18 +116,44 @@ async function mintNFTs(amount, gasLimit=GAS_LIMIT) {
     // https://docs.ethers.io/v5/api/utils/transactions/
     // https://stackoverflow.com/questions/68198724/how-would-i-send-an-eth-value-to-specific-smart-contract-function-that-is-payabl
     CONTRACT.mintNFTs(2, {value: ethers.utils.parseEther(totalCost), gasLimit: GAS_LIMIT, type: TRANSACTION_TYPE}).then(function (transactionResponse) {
-        alert('created NFT transaction');
-
         // wait for the event to respond
         transactionResponse.wait(CONFIRMED_BLOCKS).then(function (transactionReceipt) {
-            alert('NFT creation confirmed!');
-            // once we have the receipt, we can show the image
+            alert("Congratulations on your purchase of " + amount + "Avvenire Citizens!");
 
         });
 
     });
 }
 
+async function load_document()
+{
+    // get the mint price --> sets global variable
+    await getMintPrice();
+
+    // if the mint start time is past the current time, enable the button
+    CONTRACT.saleConfig().then(function (resp) {
+        if (resp[0] < Date.now())
+        {
+            // enable the button
+            $('#mint-btn').attr('class', 'btn more-btn');
+        }
+    });
+
+    // set the price
+    $('#price').text(PRICE);
+
+    setTotalCost();
+}
+
+
+// function to change the value of the total cost tag when the input box is changed
+$('#amount').on('change', function () {
+    setTotalCost();
+});
+
+
+// load the document
+load_document();
 
 // listen for event: https://docs.ethers.io/v5/api/contract/contract/#Contract--events
 
