5e89e6313905d0911f3a413119da5d8037ccaf39
Naveen Ailawadi <naveen.ailawadi91@gmail.com>
2022-04-06 14:34:43 -0400

    updated metadata

--- website/static/js/mint/mint.js
+++ website/static/js/mint/mint.js
@@ -1,7 +1,7 @@
 // need to verify with metamask: https://docs.metamask.io/guide/registering-function-names.html
 
 // get some constants
-var PRICE;  // figure out how to access structs
+var PRICE;
 
 
 // function to get the mint price
@@ -116,30 +116,66 @@ async function mintNFTs(gasLimit=GAS_LIMIT) {
     // get the amount from the input box
     var amount = $('#amount').val();
 
-    // get the gas price
-    var feeData = await CONTRACT_PROVIDER.getFeeData();  // may need to update to ethers 5.4 for this --> will see
-    var maxFeePerGas = parseInt(ethers.utils.formatUnits(feeData.maxFeePerGas, "gwei"));
-
-    // get the total cost = price * amount purchased + allowable gas cost in ETH
-    var totalCost = (PRICE * amount)/* + (maxFeePerGas * gasLimit / GWEI_PER_ETH)*/;
-
     // make the total cost a string for the parse ether utilty
     totalCost = totalCost.toString();
 
-    // call the contract from the user's current address (this is just test code)
-    // https://docs.ethers.io/v5/api/utils/transactions/
-    // https://stackoverflow.com/questions/68198724/how-would-i-send-an-eth-value-to-specific-smart-contract-function-that-is-payabl
-    CONTRACT.mintNFTs(2, {value: ethers.utils.parseEther(totalCost), gasLimit: GAS_LIMIT, type: TRANSACTION_TYPE}).then(function (transactionResponse) {
-        // wait for the event to respond
-        transactionResponse.wait(CONFIRMED_BLOCKS).then(function (transactionReceipt) {
-            alert("Congratulations on your purchase of " + amount + "Avvenire Citizens!");
-
+    // decide which contract to interact with
+    // get the sale information
+    await CONTRACT.saleConfig().then(
+        function (resp) {
+            config = resp;
         });
 
-    });
+    // check if the auction is on with saleConfig.auctionSaleStartTime <= block.time_stamp
+    if (parseFloat(ethers.utils.formatEther(config[0])) > 0)  // this will be set to 0 when the auction is over
+    {
+        // if in the future, get the start price
+        if (parseFloat(ethers.utils.formatEther(config[0])) > Date.now())
+        {
+            CONTRACT.AUCTION_START_PRICE().then(
+                function (resp) {
+                    price = resp;
+                });
+        }
+        // else, get the auction price
+        else
+        {
+            CONTRACT.getAuctionPrice().then(
+                function (resp) {
+                    price = resp;
+                });
+
+        }
+
+        // set the max and value
+        max = 3;
+        value = max;
+    }
+    // check if the user is whitelisted and the price is greater than 0
+    else if ((parseFloat(ethers.utils.formatEther(config[2])) > 0) && (whitelisted))
+    {
+        // if the user is whitelisted saleConfig.mintlistPrice
+        price = config[2];
+
+        max = 2;
+        value = max;
+    }
+    // else, use the public sale price by calling saleConfig.publicPrice
+    else
+    {
+        price = config[3];
+
+        max = 2000;
+        value = 5;
+    }
+
+
+    await CONTRACT.mintNFTs(2, {value: ethers.utils.parseEther(totalCost), gasLimit: gasLimit});
+
+    alert("Congratulations on your purchase of " + amount + "Avvenire Citizens!")
 }
 
-async function load_document()
+async function loadDocument()
 {
     // wait until you are connected
     const provider = new ethers.providers.Web3Provider(window.ethereum);
@@ -171,7 +207,7 @@ $('#amount').on('change', function () {
 
 
 // load the document
-load_document();
+loadDocument();
 
 // listen for event: https://docs.ethers.io/v5/api/contract/contract/#Contract--events
 
